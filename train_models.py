d6cae7e38f0c65e4c353d6979fe84e758f78e1cd
Matthew Dutson <mattdutson@icloud.com>
2019-04-19 14:33:13 -0500

    More model parameters specified

--- train_models.py
+++ train_models.py
@@ -28,32 +28,41 @@ if __name__ == '__main__':
 
     print('Training Doc2Vec model...')
     doc2vec = Doc2Vec(docs,
-        epochs=5,       # Number of training epochs
-        min_count=5,    # Minimum count of words to consider
-        vector_size=20, # Dimensionality of vectors
-        workers=10)     # Number of worker threads
+        alpha=0.025,        # The initial learning rate
+        epochs=5,           # Number of training epochs
+        min_alpha=0.0001,   # Learning rate linearly drops to this value
+        min_count=5,        # Minimum count of words to consider
+        vector_size=20,     # Dimensionality of vectors
+        window=4,           # Max distance between current and predicted word
+        workers=10)         # Number of worker threads
     doc2vec_filename = os.path.join(MODEL_PATH, 'doc2vec')
     doc2vec.delete_temporary_training_data(keep_doctags_vectors=False)
     doc2vec.save(doc2vec_filename)
 
     print('Training Word2Vec skip-gram model...')
     word2vec_sg = Word2Vec([doc.words for doc in docs],
-        iter=5,         # Number of training epochs
-        min_count=5,    # Minimum count of words to consider
-        sg=1,           # 1 for skip-gram, 0 for CBOW
-        size=20,        # Dimensionality of vectors
-        workers=10)     # Number of worker threads   
+        alpha=0.025,        # The initial learning rate
+        iter=5,             # Number of training epochs
+        min_alpha=0.0001,   # Learning rate linearly drops to this value
+        min_count=5,        # Minimum count of words to consider
+        sg=1,               # 1 for skip-gram, 0 for CBOW
+        size=20,            # Dimensionality of vectors
+        window=4,           # Max distance between current and predicted word
+        workers=10)         # Number of worker threads   
     word2vec_sg_filename = os.path.join(MODEL_PATH, 'word2vec_sg')
     word2vec_sg.delete_temporary_training_data()
     word2vec_sg.save(word2vec_sg_filename)
 
     print('Training Word2Vec CBOW model...')
     word2vec_cbow = Word2Vec([doc.words for doc in docs],
-        iter=5,         # Number of training epochs
-        min_count=5,    # Minimum count of words to consider
-        sg=0,           # 1 for skip-gram, 0 for CBOW
-        size=20,        # Dimensionality of vectors
-        workers=10)     # Number of worker threads  
+        alpha=0.025,        # The initial learning rate
+        iter=5,             # Number of training epochs
+        min_alpha=0.0001,   # Learning rate linearly drops to this value
+        min_count=5,        # Minimum count of words to consider
+        sg=0,               # 1 for skip-gram, 0 for CBOW
+        size=20,            # Dimensionality of vectors
+        window=4,           # Max distance between current and predicted word
+        workers=10)         # Number of worker threads  
     word2vec_cbow_filename = os.path.join(MODEL_PATH, 'word2vec_cbow')
     word2vec_cbow.delete_temporary_training_data()
     word2vec_cbow.save(word2vec_cbow_filename)
